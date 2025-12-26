e7cb4c750325aa4d2cbdce0b9fed5f3de9a81ecc
Matheus Garcia <tateusg@protonmail.com>
2025-12-26 10:08:07 -0300

    General update

---
+++ func.c
@@ -0,0 +1,47 @@
+#include <stddef.h>
+
+#include "main.h"
+
+void doChangeShirt(void)
+{
+}
+
+void doChangeJet(void)
+{
+}
+
+void doChangeHair(void)
+{
+}
+
+void doChangeSkin(void)
+{
+}
+
+void doEnd(void)
+{
+}
+
+void doChangeHairstyle(void)
+{
+}
+
+void doChangeSecWeapon(void)
+{
+}
+
+void doChangeHeadstyle(void)
+{
+}
+
+void doChangeChainstyle(void)
+{
+}
+
+void doChangeName(void)
+{
+}
+
+void doChangePants(void)
+{
+}
