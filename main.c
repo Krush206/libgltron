deedee92642d6d4e3a8efe04bfdb2c57b759f153
Matheus Garcia <tateusg@protonmail.com>
2025-12-29 18:33:34 -0300

    COPYING

--- main.c
+++ main.c
@@ -1,3 +1,25 @@
+/*
+ * SPDX-License-Identifier: 0BSD
+ *
+ * This file is derived from GLtron 0.53 by Andreas Umbach.
+ * The original author explicitly permitted unrestricted use
+ * of the code and expressed intent to place it in the public domain.
+ *
+ * Copyright (C) 2025 Matheus Garcia
+ * 
+ * Permission to use, copy, modify, and/or distribute this software
+ * for any purpose with or without fee is hereby granted.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL
+ * WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL
+ * THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR
+ * CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM
+ * LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT,
+ * NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN
+ * CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
 #include <AL/alut.h>
 #include <GL/glut.h>
 #include <stdio.h>
